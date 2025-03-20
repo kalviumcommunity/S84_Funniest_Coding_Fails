eaa545839c6ea76a7a02e9f0c5b7def375a4b643
akankshahu <akanksha.kumari.s84@kalvium.community>
2025-03-20 10:29:30 +0530

    yes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,17 @@
+import React from "react";
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import Home from "./Pages/Homepage";
 
-import './App.css';
-import CodingFail from "./CodingFail";
-import { useEffect,useState } from 'react';
-import axios from 'axios';
-
-
-function App() {
-  const [codingFails,setCodingFails]=useState([]);
-  useEffect(()=>{
-    axios.get('http://localhost:5000/coding-fails')
-    .then(response =>{
-      setCodingFails(response.data);
-
-    })
-    .catch(error=>{
-      console.error('Error fetching data:',error);
-
-    });
-  },[]);
-
+const App = () => {
   return (
-    <>
-      <header>
-        <h1>Welcome to S84 Funniest Coding Fails</h1>
-        <p>Your go-to place for hilarious coding moments!</p>
-      </header>
-      <main>
-        <section>
-          <h2>About Our Project</h2>
-          <p>
-            This project is a collection of the funniest and most relatable coding fails. Whether you're a beginner or a seasoned developer, you'll find something to laugh about here.
-          </p>
-        </section>
-        <section>
-          <h2>Funny Coding Fails</h2>
-          {codingFails.map((fail, index) => (
-            <CodingFail key={index} title={fail.title} description={fail.description} />
-          ))}
-        </section>
-      </main>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      </Router>
+
+  )
+
 }
 
 export default App;
